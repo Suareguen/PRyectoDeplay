b55e6cd68ddb9b44d4624220053289aa2f0b3e6d
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-08-10 15:40:45 +0100

    <sdvsdf

--- database/index.js
+++ database/index.js
@@ -4,9 +4,11 @@ const connection = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
+
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.DE_PORT
   }
 )
 
